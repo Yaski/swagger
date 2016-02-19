b2e9bf72515ecc771048949c610dc223943eba9a
egimaben <egimaben@gmail.com>
2016-02-19 20:33:09 +0300

    unit tests added with swagger-test module

---
+++ test.js
@@ -0,0 +1,22 @@
+var swaggerTest=require('swagger-test');
+var fs=require('fs');
+var preq=require('preq');
+var assert = require('chai').assert;
+var buffer=fs.readFileSync('cb.json');
+var swaggerSpec=JSON.parse(buffer);
+var xamples=swaggerTest.parse(swaggerSpec);
+
+
+describe('CloudBoost swagger-test',
+function(){
+  xamples.forEach(function(xample){
+    it(xample.description,
+    function(){
+      this.timeout(10000);
+     return preq[xample.request.method](xample.request).then(function(response){
+        assert.equal(response.status,xample.response.status,'wrong response received');
+      });
+    });
+  });
+});
+

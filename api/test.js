72fa37a415cea7cbcd42543894e94b88010256e0
egimaben <egimaben@gmail.com>
2016-02-23 13:29:34 +0300

    swagger-ui complete, immediately deployable with context /api

--- api/test.js
+++ api/test.js
@@ -2,12 +2,12 @@ var swaggerTest=require('swagger-test');
 var fs=require('fs');
 var preq=require('preq');
 var assert = require('chai').assert;
-var buffer=fs.readFileSync('cb.json');
+var buffer=fs.readFileSync('cloudboost-spec.json');
 var swaggerSpec=JSON.parse(buffer);
 var xamples=swaggerTest.parse(swaggerSpec);
 
 
-describe('CloudBoost swagger-test',
+describe('RESTFUL TESTS:',
 function(){
   xamples.forEach(function(xample){
     it(xample.description,
